4433b9fb173e18d0d8476e42e3d57ebc8f0043c2
Kapil Bhise <56427251+kapilbhise@users.noreply.github.com>
2020-05-30 12:46:50 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes"
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(entry=>(<Note 
+        id={entry.id}
+        title={entry.title}
+        content={entry.content}
+    />))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
